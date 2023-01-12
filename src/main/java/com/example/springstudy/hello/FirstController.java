f277d07e954e5a2d80dc6a898b99aa04a153f4fc
codesche <codesche@gmail.com>
2023-01-13 00:26:22 +0900

    feat : [002-클라이언트 요청에 대한 주소와 문자열을 리턴하는 함수 작성]

--- src/main/java/com/example/springstudy/hello/FirstController.java
+++ src/main/java/com/example/springstudy/hello/FirstController.java
@@ -2,6 +2,7 @@ package com.example.springstudy.hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 public class FirstController {
 
@@ -17,6 +18,23 @@ public class FirstController {
     @RequestMapping(value = "/first-url", method = RequestMethod.GET)
     public void first() {
 
+    }
+
+    /**
+     2. 클라이언트 요청에 대한 주소와 문자열을 리턴하는 함수를 작성
+     [조건]
+     - 컨트롤러 인식을 위한 Controller 어노테이션 이용
+     - 주소매핑은 RequestMappding을 이용
+     - HTTP 메소드는 GET 방식
+     - 요청 주소는 "/helloworld"
+     - 리턴값은 "hello world" 문자열 리턴
+     - 주소는 "/helloworld"
+     */
+    @ResponseBody
+    @RequestMapping("/helloworld")
+    public String helloWorld() {
+
+        return "hello world";
     }
 
 }
